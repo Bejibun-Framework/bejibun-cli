fa6eac99ed9c30b21cd8e8e39fa8bfe37b4ad4ff
Havea Crenata <havea.crenata@gmail.com>
2026-02-02 17:08:29 +0700

    feat: adjust UI/UX for installation

--- bin/configure.ts
+++ bin/configure.ts
@@ -1,8 +1,10 @@
 #!/usr/bin/env bun
 import Logger from "@bejibun/logger";
+import Chalk from "@bejibun/logger/facades/Chalk";
 import {isEmpty, isCommandExists} from "@bejibun/utils";
 import {rmSync} from "fs";
 import path from "path";
+import ora from "ora";
 
 const context: string = "Bejibun";
 
@@ -23,56 +25,35 @@ if (isEmpty(directory)) {
     process.exit(1);
 }
 
-Logger.separator();
-Logger.setContext(context).info("Cloning the Project");
-Logger.separator();
-
-Bun.spawnSync(["git", "clone", "https://github.com/Bejibun-Framework/bejibun.git", directory], {
-    stdin: "inherit",
-    stdout: "inherit",
-    stderr: "inherit"
-});
-
-Logger.empty();
-Logger.separator();
-Logger.setContext(context).info("Installing Dependencies");
-Logger.separator();
+const spinnerPullStarterKit = ora(Chalk.setValue(" Pulling starter kit...").inline().warn().show()).start();
+Bun.spawnSync(["git", "clone", "https://github.com/Bejibun-Framework/bejibun.git", directory]);
+spinnerPullStarterKit.succeed(Chalk.setValue(" Pulling starter kit.").inline().warn().show());
 
 const workingDirectory: string = path.resolve(process.cwd(), directory);
 
+const spinnerInstallDependencies = ora(Chalk.setValue(" Installing dependencies...").inline().warn().show()).start();
 Bun.spawnSync(["bun", "install"], {
-    cwd: workingDirectory,
-    stdin: "inherit",
-    stdout: "inherit",
-    stderr: "inherit"
+    cwd: workingDirectory
 });
+spinnerInstallDependencies.succeed(Chalk.setValue(" Installing dependencies.").inline().warn().show());
 
-Logger.empty();
-Logger.separator();
-Logger.setContext(context).info("Cleansing");
-Logger.separator();
-
+const spinnerCleansing = ora(Chalk.setValue(" Cleansing...").inline().warn().show()).start();
 for (const clean of [".git", "CHANGELOG.md", "LICENSE", "ROADMAP.md"]) {
-    Logger.setContext("Cleansing").debug(`Removing ${clean}`);
     rmSync(path.resolve(workingDirectory, clean), {
         recursive: true,
         force: true
     });
 }
+spinnerCleansing.succeed(Chalk.setValue(" Cleansing.").inline().warn().show());
 
-Logger.empty();
-Logger.separator();
-Logger.setContext(context).info("Setup Environment");
-Logger.separator();
-
-Logger.setContext("Environment").debug("Copying .env.example to .env");
-Logger.setContext("Environment").debug("Set app name");
+const spinnerSetupEnvironment = ora(Chalk.setValue(" Setup environment...").inline().warn().show()).start();
 await Bun.write(
     path.resolve(workingDirectory, ".env"),
     (
         await Bun.file(path.resolve(workingDirectory, ".env.example")).text()
     ).replaceAll("Bejibun", directory)
 );
+spinnerSetupEnvironment.succeed(Chalk.setValue(" Setup environment.").inline().warn().show());
 
 Logger.empty();
-Logger.setContext(context).info("Your framework is ready.., Happy Coding!.");
+console.log(Chalk.setValue("Success! Project initialization completed.").inline().debug().show());
